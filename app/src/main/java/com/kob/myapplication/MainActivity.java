cdcacc9ebd5363423123219b8ccd33b48058a420
wweess <861516202@qq.com>
2016-06-30 17:00:49 +0800

    新的

--- app/src/main/java/com/kob/myapplication/MainActivity.java
+++ app/src/main/java/com/kob/myapplication/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+/*
 
+ */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
